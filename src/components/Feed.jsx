a0aeb1da8a8c6bd31c083e1052a1ebb8553ed7db
UsamaXwati <usamaxwati1@gmail.com>
2023-03-05 23:53:25 +0500

    feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,7 +5,9 @@ import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
 
-  
+  useEffect(()=>{
+    fetchFromAPI(`search?part=snippet&q={selectedCategory}`)
+  }, []);
   return (
     <Stack sx={{flexDirection: { sx: "column", md: "row" }}}>
       <Box sx={{height: { sx: "auto", md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2}}}>
